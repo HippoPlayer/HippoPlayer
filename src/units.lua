692b32833c8bc2a7a937be9543303e1ba3dc3ce8
Daniel Collin <daniel@collin.com>
2011-08-12 22:07:56 +0200

    Added SDL (doesn't fully build yet)

--- src/units.lua
+++ src/units.lua
@@ -1,13 +1,33 @@
 
 StaticLibrary {
 	Name = "zlib",
-	 Sources = { Glob { Dir = "thirdparty/zlib-1.2.5", Extensions = { ".c" } } },
+	Sources = { Glob { Dir = "thirdparty/zlib-1.2.5", Extensions = { ".c" } } },
+}
+
+StaticLibrary {
+	Name = "sdl",
+
+	Env = {
+		CPPPATH = { "thirdparty/sdl-1.2.14/include" },
+	},
+	
+	Sources = {
+		FGlob {
+			Dir = "thirdparty/sdl-1.2.14",
+			Extensions = { ".c" },
+			Filters = {
+				{ Pattern = "win32"; Config = "win32-*-*" },
+				{ Pattern = "macosx"; Config = "macosx-*-*" },
+				{ Pattern = "bsd"; Config = "bsd-*-*" },
+			}
+		}
+	}
 }
 
 Program {
 	Name = "player",
 	Sources = { "hippoplayer/player/main.c" },
-	Depends = { "zlib" },
+	Depends = { "zlib", "sdl" },
 }
 
 Default "player"
