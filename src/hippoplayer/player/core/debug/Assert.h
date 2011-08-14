7995fd5fed87c9be120ac403e60dc356613fee5c
Daniel Collin <daniel@collin.com>
2011-08-14 22:40:34 +0200

    Make sure assert do something useful

--- src/hippoplayer/player/core/debug/Assert.h
+++ src/hippoplayer/player/core/debug/Assert.h
@@ -5,31 +5,34 @@
 
 #include "../Types.h"
 
+void Hippo_displayAssert();
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define ZENIC_ASSERT(exp) ZENIC_ASSERT_DESC(exp, #exp)
-#define ZENIC_ASSERT_DESC(exp, ...) \
+#define HIPPO_ASSERT(exp) HIPPO_ASSERT_DESC(exp, #exp)
+#define HIPPO_ASSERT_DESC(exp, ...) \
 do\
 {\
-	if (ZENIC_UNLIKELY(!(exp)))\
+	if (HIPPO_UNLIKELY(!(exp)))\
 	{\
+		Hippo_displayAssert(); \
 	}\
 }\
 while (0);
 
-#define ZENIC_ASSERT_CONCAT_(a, b) a##b
-#define ZENIC_ASSERT_CONCAT(a, b) ZENIC_ASSERT_CONCAT_(a, b)
+#define HIPPO_ASSERT_CONCAT_(a, b) a##b
+#define HIPPO_ASSERT_CONCAT(a, b) HIPPO_ASSERT_CONCAT_(a, b)
 // These can't be used after statements in c89
 #ifdef __COUNTER__
-	#define ZENIC_STATIC_ASSERT(e,m) \
-		enum { ZENIC_ASSERT_CONCAT(static_assert_, __COUNTER__) = 1/(!!(e)) }
+	#define HIPPO_STATIC_ASSERT(e,m) \
+		enum { HIPPO_ASSERT_CONCAT(static_assert_, __COUNTER__) = 1/(!!(e)) }
 #else
 	// This can't be used twice on the same line so ensure if using in headers
 	// that the headers are not included twice (by wrapping in #ifndef...#endif)
 	// Note it doesn't cause an issue when used on same line of separate modules
 	// compiled with gcc -combine -fwhole-program.  
-	#define ZENIC_STATIC_ASSERT(e,m) \
-		enum { ZENIC_ASSERT_CONCAT(assert_line_, __LINE__) = 1/(!!(e)) }
+	#define HIPPO_STATIC_ASSERT(e,m) \
+		enum { HIPPO_ASSERT_CONCAT(assert_line_, __LINE__) = 1/(!!(e)) }
 #endif
 
 #endif
