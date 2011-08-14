46106d8b5a25c81622b1c037b8b0d196172d44ab
Daniel Collin <daniel@collin.com>
2011-08-14 20:08:26 +0200

    Added HIPPO_MACOSX to Mac OS X and HIPPO_WIN32 for Win32

--- src/tundra.lua
+++ src/tundra.lua
@@ -8,11 +8,17 @@ Build {
 			Name = "macosx-gcc",
 			DefaultOnHost = "macosx",
 			Tools = { "clang-osx" },
+			Env = {
+				CPPDEFS = { "HIPPO_MACOSX" },
+			},
 		},
 		{
 			Name = "win32-msvc",
 			DefaultOnHost = "windows",
 			Tools = { "msvc-vs2008" },
+			Env = {
+				CPPDEFS = { "HIPPO_WIN32" },
+			},
 		},
 	},
 }
