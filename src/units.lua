2d291e83816130d6ea00105c38a7cb010087e2ea
Daniel Collin <daniel@collin.com>
2011-08-14 19:51:59 +0200

    Removed the remaining bits of SDL

--- src/units.lua
+++ src/units.lua
@@ -4,33 +4,10 @@ StaticLibrary {
 	Sources = { Glob { Dir = "thirdparty/zlib-1.2.5", Extensions = { ".c" } } },
 }
 
-StaticLibrary {
-	Name = "sdl",
-
-	Env = {
-		CPPPATH = { "thirdparty/SDL-1.3.0-5605/include" },
-		CCOPTS = {
-			{ "/MD"; Config = "*-msvc-debug" },
-		}
-	},
-	
-	Sources = {
-		FGlob {
-			Dir = "thirdparty/SDL-1.3.0-5605",
-			Extensions = { ".c" },
-			Filters = {
-				{ Pattern = "windows"; Config = "win32-*-*" },
-				{ Pattern = "macosx"; Config = "macosx-*-*" },
-			}
-		}
-	}
-}
-
 Program {
 	Name = "player",
 
 	Env = {
-		CPPPATH = { "thirdparty/SDL-1.3.0-5605/include" },
 		CCOPTS = {
 			{ "/MD"; Config = "*-msvc-debug" },
 		},
@@ -44,8 +21,10 @@ Program {
 		{ "user32.lib", "imm32.lib", "gdi32.lib", "ole32.lib", "version.lib", "shell32.lib", "Winmm.lib"; Config = "win*-*-*" }, 
 	},
 
+	Frameworks = { "Cocoa", "AudioToolbox", "CoreAudio" },
+
 	Sources = { "hippoplayer/player/main.c" },
-	Depends = { "zlib", "sdl" },
+	Depends = { "zlib" },
 }
 
 Default "player"
