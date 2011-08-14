c35800c37bdde2c11d3eb00dbcf1961aa6449603
Daniel Collin <daniel@collin.com>
2011-08-14 22:44:48 +0200

    Basic setup to create window and just wait around

--- src/hippoplayer/player/main.c
+++ src/hippoplayer/player/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "graphics/HippoWindow.h"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
+	struct HippoWindow* window = HippoWindow_create(0, "foo", 0, 0);
+
+	while (!HippoWindow_isClosed())
+	{
+		HippoWindow_updateEvents();
+	}
+
 	return 0;
 }
