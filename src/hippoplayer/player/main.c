f04f2f2d02a711cc08d7a3fa3959d58b3f983450
Daniel Collin <daniel@collin.com>
2011-08-15 16:33:59 +0200

    Misc setup and WinMain for win32 version

--- src/hippoplayer/player/main.c
+++ src/hippoplayer/player/main.c
@@ -3,11 +3,31 @@
 #include <string.h>
 #include "graphics/HippoWindow.h"
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#if defined(HIPPO_WIN32)
+#include <windows.h>
+#endif
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#if defined(HIPPO_WIN32)
+extern HINSTANCE g_winInstance; 
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	HippoWindowRect rect;
+	struct HippoWindow* window;
+	g_winInstance = hInstance;
+#else
 int main()
 {
-	struct HippoWindow* window = HippoWindow_create(0, "foo", 0, 0);
+	struct HippoWindow* window;
+#endif
+
+	rect.x = 50;
+	rect.y = 50;
+	rect.width = 250;
+	rect.height = 250;
+
+	window = HippoWindow_create(0, "foo", 0, &rect);
 
 	while (!HippoWindow_isClosed())
 	{
