45ec3f5ff24884c4dc0104a11158d87f49dc9f4b
Daniel Collin <daniel@collin.com>
2011-08-17 21:32:37 +0200

    Added HippoImageLoader which is a wrapper for loading images from disk and from memory (useful for zip support later on)

---
+++ src/hippoplayer/player/graphics/HippoImageLoader.h
@@ -0,0 +1,25 @@
+
+#ifndef HIPPOIMAGE_H
+#define HIPPOIMAGE_H
+
+#include <core/Types.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+typedef struct HippoImage
+{
+	const uint32_t* data;
+	int width;
+	int height;
+	int comp;
+	bool hasAlpha;
+
+} HippoImage;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool HippoImageLoader_loadFile(HippoImage* image, const char* filename);
+bool HippoImageLoader_loadBuffer(HippoImage* image, const void* buffer, size_t size);
+
+#endif
+
