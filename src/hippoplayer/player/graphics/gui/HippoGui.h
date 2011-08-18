d6838f4922fb953673638a25fb8e8a418ba04319
Daniel Collin <daniel@collin.com>
2011-08-18 21:52:28 +0200

    Setting up structures for the UI

--- src/hippoplayer/player/graphics/gui/HippoGui.h
+++ src/hippoplayer/player/graphics/gui/HippoGui.h
@@ -26,6 +26,9 @@ extern HippoGuiState g_hippoGuiState;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+void HippoGui_begin();
+void HippoGui_end();
+
 void HippoGui_beginVerticalStackPanelXY(int x, int y);
 void HippoGui_beginHorizontalStackPanelXY(int x, int y);
 
@@ -35,10 +38,14 @@ void HippoGui_beginHorizontalStackPanel();
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Different controlls and gui functions
 
+void HippoGui_staticImage(struct HippoImage* image);
 void HippoGui_fill(uint32_t color, uint32_t x, uint32_t y, uint32_t w, uint32_t h);
 
-bool HippoGui_button(const char* text, int x, int y);
-bool HippoGui_buttonImage(const char* text, int x, int y);
+bool HippoGui_buttonCoords(const char* text, int x, int y);
+bool HippoGui_buttonCoordsImage(const char* text, int x, int y);
+
+bool HippoGui_button(const char* text);
+bool HippoGui_buttonImage(struct HippoImage* image);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
