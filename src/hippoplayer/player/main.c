17724159e4ff6879e17003580c6f4ddc15490d7d
Daniel Collin <daniel@collin.com>
2011-08-21 20:59:53 +0200

    Now using classic gui (a bit hackyish but will do for now)

--- src/hippoplayer/player/main.c
+++ src/hippoplayer/player/main.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "graphics/HippoWindow.h"
+#include "graphics/HippoImageLoader.h"
+#include "graphics/gui/HippoGui.h"
 
 #if defined(HIPPO_WIN32)
 #include <windows.h>
 #endif
+
+extern void HippoGui_drawClassic();
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #if defined(HIPPO_WIN32)
@@ -23,15 +27,30 @@ int main()
 	struct HippoWindow* window;
 #endif
 
+	/*
+	HippoImage image;
+
+	if (!HippoImageLoader_loadFile(&image, "test.png"))
+	{
+		printf("Failed to open %s\n", "test.png");
+	}
+	else
+	{
+		printf("0x%p %d %d %d\n", image.data, image.width, image.height, image.comp);
+	}
+	*/
+
 	rect.x = 50;
 	rect.y = 50;
 	rect.width = 250;
 	rect.height = 250;
 
 	window = HippoWindow_create(0, "foo", 0, &rect);
+	HippoGui_reset();
 
 	while (!HippoWindow_isClosed())
 	{
+		HippoGui_drawClassic();
 		HippoWindow_updateEvents();
 	}
 
