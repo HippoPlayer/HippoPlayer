f39aa5e64ba7b278baad399c14bab29a53ab3e5d
Daniel Collin <daniel@collin.com>
2011-08-21 20:27:19 +0200

    Use new drawBorder code

--- src/hippoplayer/player/graphics/gui/HippoClassicTest.c
+++ src/hippoplayer/player/graphics/gui/HippoClassicTest.c
@@ -11,14 +11,11 @@ void HippoGui_drawClassic()
 	// Some initial drawing
 
 	HippoGui_fill(0x00a0a0a0, 0, 0, 264, 136);
-	HippoGui_fill(0x00ffffff, 25, 54, 1, 68);
-	HippoGui_fill(0x00ffffff, 25, 54, 224, 1);
-	HippoGui_fill(0x00000000, 25 + 224, 54, 1, 68);
-	HippoGui_fill(0x00000000, 25, 62 + 60, 224, 1);
-	HippoGui_fill(0x00aaaaaa, 26, 55, 157, 67);
+	HippoGui_drawBorder(0x00ffffff, 0x00000000, 25, 65, 224, 67);
+	HippoGui_fill(0x00aaaaaa, 26, 66, 157, 67);
 
 	// todo: Add support for staticImage with coords
-	HippoGui_beginHorizontalStackPanelXY(158, 56);
+	HippoGui_beginHorizontalStackPanelXY(158, 66);
 	HippoGui_staticImage("skins/classic/hippo.png");
 
 	HippoGui_beginHorizontalStackPanelXY(0, 0);
@@ -29,7 +26,7 @@ void HippoGui_drawClassic()
 		HippoGui_staticImage("skins/classic/minimize.png");
 	}
 
-	HippoGui_beginHorizontalStackPanelXY(0, 30);
+	HippoGui_beginHorizontalStackPanelXY(0, 38);
 	{
 		if (HippoGui_buttonImage("skins/classic/play.png"))
 			printf("play!\n");
