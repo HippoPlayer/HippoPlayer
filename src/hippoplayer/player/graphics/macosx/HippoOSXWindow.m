70331f99cf8a4c13851082ebe3418be0c4390ee7
Daniel Collin <daniel@collin.com>
2011-08-21 16:35:19 +0200

    Moved close button to upper left corner

--- src/hippoplayer/player/graphics/macosx/HippoOSXWindow.m
+++ src/hippoplayer/player/graphics/macosx/HippoOSXWindow.m
@@ -105,8 +105,8 @@
 		[super setContentView:frameView];
 
 		closeButton = [NSWindow standardWindowButton:NSWindowCloseButton forStyleMask:NSTitledWindowMask];
-		//NSRect closeButtonRect = [closeButton frame];
-		//[closeButton setFrame:NSMakeRect(WINDOW_FRAME_PADDING - 20, bounds.size.height - (WINDOW_FRAME_PADDING - 20) - closeButtonRect.size.height, closeButtonRect.size.width, closeButtonRect.size.height)];
+		NSRect closeButtonRect = [closeButton frame];
+		[closeButton setFrame:NSMakeRect(0, bounds.size.height - closeButtonRect.size.height, closeButtonRect.size.width, closeButtonRect.size.height)];
 		[closeButton setAutoresizingMask:NSViewMaxXMargin | NSViewMinYMargin];
 		[frameView addSubview:closeButton];
 	}
