c8c84f24684d306c53ba73412f4d187932877d25
Daniel Collin <daniel@collin.com>
2011-08-21 16:44:10 +0200

    More testing on of the UI

--- src/hippoplayer/player/graphics/gui/HippoClassicTest.c
+++ src/hippoplayer/player/graphics/gui/HippoClassicTest.c
@@ -1,12 +1,22 @@
 #include "HippoClassicTest.h"
-#include <graphics/gui/HippoGui.h>
+#include "graphics/gui/HippoGui.h"
+#include <stdio.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void HippoGui_drawClassic()
 {
+	HippoGui_begin();
 
+	HippoGui_beginHorizontalStackPanelXY(0, 40);
+	{
+		if (HippoGui_buttonImage("skins/classic/play.png"))
+			printf("play!\n");
+		//if (HippoGui_buttonImage("skins/classic/open.png"))
+		//	printf("open!\n");
+	}
 
+	HippoGui_end();
 }
 
 
