04a44ebcb41b3a8603fbf7721eff4e0708006ae3
Daniel Collin <daniel@collin.com>
2011-08-22 23:59:38 +0200

    Added sinwave audio test

--- src/hippoplayer/player/main.c
+++ src/hippoplayer/player/main.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "audio/HippoAudio.h"
 #include "graphics/HippoWindow.h"
 #include "graphics/HippoImageLoader.h"
 #include "graphics/gui/HippoGui.h"
+#include "plugin_api/HippoPlugin.h"
+#include "audio/HippoAudio.h"
+#include <math.h>
 
 #if defined(HIPPO_WIN32)
 #include <windows.h>
 #endif
 
 extern void HippoGui_drawClassic();
+static HippoAudioDevice g_audioDevices[64];
+
+struct MyData
+{
+	float sinWave;
+};
+
+static int readData(void* userData, void* dest, int size)
+{
+	struct MyData* data = (struct MyData*)userData;
+	float* destOut = (float*)dest;
+
+	for (int i = 0; i < size; ++i)
+	{
+		data->sinWave += 0.01f;
+		*destOut++ = (float)sin(data->sinWave);
+	}
+
+	return size;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #if defined(HIPPO_WIN32)
@@ -26,6 +50,22 @@ int main()
 	HippoWindowRect rect;
 	struct HippoWindow* window;
 #endif
+
+	int deviceCount = HippoAudio_buildDeviceList(g_audioDevices, 64);
+
+	for (int i = 0; i < deviceCount; ++i)
+	{
+		printf("name %s id %p\n", g_audioDevices[i].name, g_audioDevices[i].deviceId);
+	}
+
+	struct MyData data;
+	data.sinWave = 0.0f;
+
+	HippoPlaybackPlugin plugin;
+	plugin.userData = &data;
+	plugin.readData = readData;
+
+	HippoAudio_openDefaultOutput(&plugin);
 
 	/*
 	HippoImage image;
@@ -53,6 +93,8 @@ int main()
 		HippoGui_drawClassic();
 		HippoWindow_updateEvents();
 	}
+
+	HippoAudio_close();
 
 	return 0;
 }
