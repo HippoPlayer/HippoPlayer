8b10ff29e84dbec77eff69b955c76214200e21ba
Daniel Collin <daniel@collin.com>
2011-08-22 23:59:15 +0200

    Definition of a HippoPlaybackPlugin

---
+++ src/hippoplayer/player/plugin_api/HippoPlugin.h
@@ -0,0 +1,23 @@
+
+#ifndef HIPPOAPI_H
+#define HIPPOAPI_H
+
+typedef struct HippoPlaybackPlugin
+{
+	int version;
+	const char* (*info)(void* userData);
+	const char* (*trackInfo)(void* userData);
+	const char* (*supportedExtensions)(void* userData);
+	int (*create)(void* userData);
+	int (*destroy)(void* userData);
+	int (*open)(void* userData, const char* buffer);
+	int (*close)(void* userData);
+	int (*readData)(void* userData, void* dest, int size);
+	int (*seek)(void* userData, int ms);
+
+	void* userData;
+
+} HippoPlaybackPlugin;
+
+#endif
+
