78b66d59b84059a24359d07f05ea634916767bd0
Daniel Collin <daniel@collin.com>
2017-10-07 20:42:57 +0200

    Custom source playback

--- src/hippo_player/src/main.rs
+++ src/hippo_player/src/main.rs
@@ -7,6 +7,64 @@ use minifb::{Key, WindowOptions, Window};
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
+use std::time::Duration;
+
+/// An infinite source that produces a sine.
+///
+/// Always has a rate of 48kHz and one channel.
+#[derive(Clone, Debug)]
+pub struct HippoPlayback {
+    freq: f32,
+    num_sample: usize,
+}
+
+impl HippoPlayback {
+    /// The frequency of the sine.
+    #[inline]
+    pub fn new(freq: u32) -> HippoPlayback {
+        HippoPlayback {
+            freq: freq as f32,
+            num_sample: 0,
+        }
+    }
+}
+
+impl Iterator for HippoPlayback {
+    type Item = f32;
+
+    #[inline]
+    fn next(&mut self) -> Option<f32> {
+        self.num_sample = self.num_sample.wrapping_add(1);
+
+        //println!("next sample!");
+
+        let value = 2.0 * 3.14159265 * self.freq * self.num_sample as f32 / 48000.0;
+        Some(value.sin())
+    }
+}
+
+impl Source for HippoPlayback {
+    #[inline]
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
+
+    #[inline]
+    fn channels(&self) -> u16 {
+        1
+    }
+
+    #[inline]
+    fn samples_rate(&self) -> u32 {
+        48000
+    }
+
+    #[inline]
+    fn total_duration(&self) -> Option<Duration> {
+        None
+    }
+}
+
 struct HippoPlayer {
     window: minifb::Window,
     audio_endpoint: rodio::Endpoint,
@@ -29,7 +87,7 @@ impl HippoPlayer {
         let sink = Sink::new(&endpoint);
 
         // Add a dummy source of the sake of the example.
-        let source = rodio::source::SineWave::new(440);
+        let source = HippoPlayback::new(440);
         sink.append(source);
 
         HippoPlayer {
@@ -45,6 +103,8 @@ impl HippoPlayer {
             for i in self.buffer.iter_mut() {
                 *i = 0; // write something more funny here!
             }
+
+            println!("window update!");
 
             // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
             self.window.update_with_buffer(&self.buffer).unwrap();
