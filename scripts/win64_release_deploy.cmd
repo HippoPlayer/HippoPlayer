b1e0b26465212f6dc2f2fb23aa18d4cfbfcf43a1
Daniel Collin <daniel@collin.com>
2018-01-22 15:39:00 +0100

    Created Windows deploy scripts

---
+++ scripts/win64_release_deploy.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+cd t2-output
+
+IF NOT "%BUILD_FILE_ID%" == "" GOTO BuildIdSet
+
+SET BUILD_FILE_ID=%DATE:~10,4%%DATE:~7,2%%DATE:~4,2%_%TIME:~0,2%%TIME:~3,2%
+
+:BuildIdSet:
+
+rmdir /s /q HippoPlayer
+rmdir /s /q build
+mkdir build
+mkdir HippoPlayer
+mkdir HippoPlayer\platforms
+
+copy %QT5%\bin\Qt5Core.dll HippoPlayer
+copy %QT5%\bin\Qt5Widgets.dll HippoPlayer
+copy %QT5%\bin\Qt5Gui.dll HippoPlayer
+copy %QT5%\plugins\platforms\qwindows.dll HippoPlayer\platforms
+copy win64-msvc-release-default\hippo_player.exe HippoPlayer
+copy win64-msvc-release-default\*.dll HippoPlayer
+
+7z a build\hippoplayer_win_%BUILD_FILE_ID%.7z HippoPlayer
+
+cd ..
