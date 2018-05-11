4447b747b17e2ff1baaa21a075b0d9d7142ab696
Daniel Collin <daniel@collin.com>
2018-05-11 12:25:52 +0200

    WIP on PlayerView

--- src/plugins/view/player/src/lib.rs
+++ src/plugins/view/player/src/lib.rs
@@ -14,38 +14,77 @@ use hippo_api::view::View;
 use hippo_api::service::Service;
 
 struct Playlist {
-    widget: Option<PushButton>,
-    ui: Option<PluginUi>,
+    ui: PluginUi,
+    prev_button: PushButton,
+    stop_button: PushButton,
+    play_button: PushButton,
+    next_button: PushButton,
 }
 
 impl View for Playlist {
     fn new(_service: &Service) -> Playlist {
         Playlist {
-            widget: None,
-            ui: None,
+            ui: PluginUi { obj: None },
+			prev_button: PushButton { obj: None },
+			stop_button: PushButton { obj: None },
+			play_button: PushButton { obj: None },
+			next_button: PushButton { obj: None },
         }
     }
 
     fn setup_ui(&mut self, ui: PluginUi) {
-        let widget = ui.create_push_button();
-        widget.set_text("player");
+        self.ui = ui;
+		self.prev_button = ui.create_push_button();
+		self.stop_button = ui.create_push_button();
+		self.play_button = ui.create_push_button();
+		self.next_button = ui.create_push_button();
 
-        /*
-        set_push_button_pressed_event!(self.player_view.prev_button, self, HippoPlayer, HippoPlayer::prev_song);
-        set_push_button_pressed_event!(self.player_view.play_button, self, HippoPlayer, HippoPlayer::play_song);
-        set_push_button_pressed_event!(self.player_view.stop_button, self, HippoPlayer, HippoPlayer::stop_song);
-        set_push_button_pressed_event!(self.player_view.next_button, self, HippoPlayer, HippoPlayer::next_song);
-        */
+        set_push_button_pressed_event!(self.player_view.prev_button, self, Playlist, HippoPlayer::prev_song);
+        set_push_button_pressed_event!(self.player_view.play_button, self, Playlist, HippoPlayer::play_song);
+        set_push_button_pressed_event!(self.player_view.stop_button, self, Playlist, HippoPlayer::stop_song);
+        set_push_button_pressed_event!(self.player_view.next_button, self, Playlist, HippoPlayer::next_song);
 
         ui.get_parent().resize(500, 500);
-
-        self.ui = Some(ui);
-        self.widget = Some(widget)
     }
+
 
     fn destroy(&mut self) {
+
     }
 }
+
+impl Player {
+	fn create_button(ui: &PlugiUi, icon_filename: &str) -> PushButton {
+        let icon = ui.create_icon();
+		let button = ui.create_push_button();
+
+        icon.add_file(icon_filename);
+		button.set_icon(&icon);
+
+		button
+	}
+
+    fn prev_song(&mut self) {
+
+    }
+
+    fn play_song(&mut self) {
+
+	}
+
+    fn stop_song(&mut self) {
+
+	}
+
+    fn next_song(&mut self) {
+
+	}
+
+
+
+}
+
+
 /*
     fn select_song(&mut self, item: &ListWidgetItem) {
         /*
