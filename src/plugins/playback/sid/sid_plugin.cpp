d4d9e183946ce3d7e5257511644d8e56d0058c1b
Daniel Collin <daniel@collin.com>
2020-07-11 07:53:15 +0200

    SID: Metadata + subsongs

--- src/plugins/playback/sid/sid_plugin.cpp
+++ src/plugins/playback/sid/sid_plugin.cpp
@@ -45,7 +45,7 @@ enum HippoProbeResult sid_probe_can_play(const uint8_t* data, uint32_t data_size
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static const char* sid_supported_extensions() {
-	return "sid,psid";
+	return "sid,psid,prg,c64,p00,str,mus";
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -174,6 +174,68 @@ static void sid_event(void* user_data, const unsigned char* data, int len) {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+static int sid_metadata(const char* url, const HippoServiceAPI* service_api) {
+    void* data = 0;
+    uint64_t size = 0;
+
+    const HippoIoAPI* io_api = HippoServiceAPI_get_io_api(service_api, HIPPO_FILE_API_VERSION);
+    const HippoMetadataAPI* metadata_api = HippoServiceAPI_get_metadata_api(service_api, HIPPO_METADATA_API_VERSION);
+
+    HippoIoErrorCode res = HippoIo_read_file_to_memory(io_api, url, &data, &size);
+
+    if (res < 0) {
+        return -1;
+    }
+
+	SidTune* tune = new SidTune((uint8_t*)data, size);
+
+    if (!tune->getStatus()) {
+        return -1;
+    }
+
+    int subsongs_count = tune->getInfo()->songs();
+    HippoMetadataId index = HippoMetadata_create_url(metadata_api, url);
+
+    char title_name[1024] = { 0 };
+
+    if (subsongs_count > 0) {
+        auto info = tune->getInfo(0);
+
+        // so this is somewhat hacky but there is no API to know what kind of strings we get back.
+        // So if we have 1 string we assume it's title, if we have 3, name, author, released
+        auto string_count = info->numberOfInfoStrings();
+        HippoMetadata_set_tag(metadata_api, index, HippoMetadata_SongTypeTag, info->formatString());
+        HippoMetadata_set_tag_f64(metadata_api, index, HippoMetadata_LengthTag, 0.0f);
+
+        if (string_count == 3) {
+            strcpy(title_name, info->infoString(0));
+            HippoMetadata_set_tag(metadata_api, index, HippoMetadata_TitleTag, info->infoString(0));
+            HippoMetadata_set_tag(metadata_api, index, HippoMetadata_ArtistTag, info->infoString(1));
+            HippoMetadata_set_tag(metadata_api, index, HippoMetadata_DateTag, info->infoString(2));
+        } else if (string_count == 1) {
+            strcpy(title_name, info->infoString(0));
+            HippoMetadata_set_tag(metadata_api, index, HippoMetadata_TitleTag, info->infoString(0));
+        } else {
+            strcpy(title_name, "Unknown");
+        }
+
+        if (subsongs_count > 1) {
+            for (int i = 0; i < subsongs_count; ++i) {
+                char subsong_name[1024] = { 0 };
+                sprintf(subsong_name, "%s (%d/%d)", title_name, i + 1, subsongs_count);
+                HippoMetadata_add_subsong(metadata_api, index, i, subsong_name, 0.0f);
+            }
+        }
+    }
+
+    // Make sure to free the buffer before we leave
+    HippoIo_free_file_to_memory(io_api, data);
+
+    return 1;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 static HippoPlaybackPlugin g_sid_plugin = {
 	HIPPO_PLAYBACK_PLUGIN_API_VERSION,
 	"SID",
@@ -187,7 +249,7 @@ static HippoPlaybackPlugin g_sid_plugin = {
 	sid_close,
 	sid_read_data,
 	sid_seek,
-	NULL,
+	sid_metadata,
 	NULL,
 	NULL,
 };
