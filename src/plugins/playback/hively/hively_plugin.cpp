5e6dfa4ae314ad96014a41a4edc379b0cb8b98dc
Daniel Collin <daniel@collin.com>
2020-07-11 08:20:08 +0200

    Hively/AHX: Fixed bad instrument name

--- src/plugins/playback/hively/hively_plugin.cpp
+++ src/plugins/playback/hively/hively_plugin.cpp
@@ -161,7 +161,8 @@ static int hively_metadata(const char* filename, const HippoServiceAPI* service_
     HippoMetadata_set_tag(metadata_api, index, HippoMetadata_AuthoringToolTag, tool);
     HippoMetadata_set_tag_f64(metadata_api, index, HippoMetadata_LengthTag, length);
 
-    for (int i = 0; i < tune->ht_InstrumentNr; ++i) {
+    // instruments starts from 1 in hively so skip 0
+    for (int i = 1; i < tune->ht_InstrumentNr; ++i) {
         HippoMetadata_add_instrument(metadata_api, index, tune->ht_Instruments[i].ins_Name);
 	}
 
