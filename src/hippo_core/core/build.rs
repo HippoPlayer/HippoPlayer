fe0e7dbd1120b11d24214e67c70d62fcb25645d7
Daniel Collin <daniel@collin.com>
2020-08-30 10:11:03 +0200

    Fixed link error when building tests

--- src/hippo_core/core/build.rs
+++ src/hippo_core/core/build.rs
@@ -4,5 +4,7 @@ fn main() {
     let env = env::var("TARGET").unwrap();
     if env.contains("darwin") {
         println!("cargo:rustc-link-lib=c++");
+    } else if env.contains("linux") {
+        println!("cargo:rustc-link-lib=static=stdc++");
     }
 }
