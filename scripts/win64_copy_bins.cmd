bb2d5d7b26aa2b7a7afca32b846bafad45efdf3c
Daniel Collin <daniel.collin@unity3d.com>
2020-12-08 21:31:15 +0100

    Script to copy needed Qt binaries

---
+++ scripts/win64_copy_bins.cmd
@@ -0,0 +1,6 @@
+@echo off
+copy %QT5_BIN%\Qt5Core.dll t2-output\win64-msvc-debug-default\Qt5Core.dll /y
+copy %QT5_BIN%\Qt5Gui.dll t2-output\win64-msvc-debug-default\Qt5Gui.dll /y
+copy %QT5_BIN%\Qt5Widgets.dll t2-output\win64-msvc-debug-default\Qt5Widgets.dll /y
+robocopy %QT5_BIN%\..\plugins t2-output\win64-msvc-debug-default\plugins /s /e
+
