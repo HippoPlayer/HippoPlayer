12f0ab483fab94afc46bb9be1e8cd6b11c7f4d93
Daniel Collin <daniel.collin@unity3d.com>
2020-12-11 08:27:35 +0100

    copy to release also

--- scripts/win64_copy_bins.cmd
+++ scripts/win64_copy_bins.cmd
@@ -4,3 +4,9 @@ copy %QT5_BIN%\Qt5Gui.dll t2-output\win64-msvc-debug-default\Qt5Gui.dll /y
 copy %QT5_BIN%\Qt5Widgets.dll t2-output\win64-msvc-debug-default\Qt5Widgets.dll /y
 robocopy %QT5_BIN%\..\plugins t2-output\win64-msvc-debug-default\plugins /s /e
 
+copy %QT5_BIN%\Qt5Core.dll t2-output\win64-msvc-release-default\Qt5Core.dll /y
+copy %QT5_BIN%\Qt5Gui.dll t2-output\win64-msvc-release-default\Qt5Gui.dll /y
+copy %QT5_BIN%\Qt5Widgets.dll t2-output\win64-msvc-release-default\Qt5Widgets.dll /y
+robocopy %QT5_BIN%\..\plugins t2-output\win64-msvc-release-default\plugins /s /e
+
+
