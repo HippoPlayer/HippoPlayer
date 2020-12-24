b6e6df4340a97c5e0a40fe0cd9b580bc65b0b3d5
Daniel Collin <daniel@collin.com>
2020-12-24 18:07:05 +0100

    Switched asap back to 44100
    And moved over to new structure that supports resampling

--- src/plugins/playback/asap/asap_plugin.c
+++ src/plugins/playback/asap/asap_plugin.c
@@ -1,11 +1,11 @@
 #include <HippoPlugin.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <asap.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define FREQ 48000
+#define FREQ 44100
 #define FRAME_SIZE 1024
 
 static const struct HippoIoAPI* g_io_api = 0;
@@ -16,12 +16,12 @@ HippoLogAPI* g_hp_log = NULL;
 static const char* find_extension(const char* path, int* offset) {
     for (size_t i = strlen(path) - 1; i > 0; i--) {
         if (path[i] == '.') {
-        	*offset = (int)(i + 1);
-        	return &path[i];
+            *offset = (int)(i + 1);
+            return &path[i];
         }
     }
 
-	*offset = 0;
+    *offset = 0;
     return path;
 }
 
@@ -29,52 +29,52 @@ static const char* find_extension(const char* path, int* offset) {
 
 static const char* find_filename_start(const char* path, int* offset) {
     for (size_t i = strlen(path) - 1; i > 0; i--) {
-    	char c = path[i];
+        char c = path[i];
         if (c == '/' || c == '\\') {
-        	*offset = (int)(i + 1);
-        	return &path[i + 1];
+            *offset = (int)(i + 1);
+            return &path[i + 1];
         }
     }
 
-	*offset = 0;
+    *offset = 0;
     return path;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct ReplayerData {
-	ASAP* song;
+    ASAP* song;
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static const char* asap_supported_extensions() {
-	return "ctc";
+    return "ctc";
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static void* asap_create(const HippoServiceAPI* service_api) {
-	void* data = malloc(sizeof(struct ReplayerData));
-	memset(data, 0, sizeof(struct ReplayerData));
+    void* data = malloc(sizeof(struct ReplayerData));
+    memset(data, 0, sizeof(struct ReplayerData));
 
     g_io_api = HippoServiceAPI_get_io_api(service_api, 1);
 
-	return data;
+    return data;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static int asap_destroy(void* user_data) {
-	struct ReplayerData* data = (struct ReplayerData*)user_data;
+    struct ReplayerData* data = (struct ReplayerData*)user_data;
 
-	if (data->song) {
-		ASAP_Delete(data->song);
-	}
+    if (data->song) {
+        ASAP_Delete(data->song);
+    }
 
-	free(data);
+    free(data);
 
-	return 0;
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -92,100 +92,90 @@ static ASAP* open_asap(const char* filename, const struct HippoIoAPI* io) {
 
     ASAP* song = ASAP_New();
 
-	if (!ASAP_Load(song, filename, (uint8_t*)data, (int)size)) {
-		ASAP_Delete(song);
-		song = NULL;
-	}
+    if (!ASAP_Load(song, filename, (uint8_t*)data, (int)size)) {
+        ASAP_Delete(song);
+        song = NULL;
+    }
 
     HippoIo_free_file_to_memory(io, data);
 
-	return song;
+    return song;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static int asap_open(void* user_data, const char* filename, int subsong) {
-	struct ReplayerData* data = (struct ReplayerData*)user_data;
+    struct ReplayerData* data = (struct ReplayerData*)user_data;
 
-	if (!(data->song = open_asap(filename, g_io_api))) {
+    if (!(data->song = open_asap(filename, g_io_api))) {
         hp_error("Unable to open: %s", filename);
-		return -1;
-	}
+        return -1;
+    }
 
     ASAP_PlaySong(data->song, subsong, -1);
 
-	return 0;
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static int asap_close(void* user_data) {
-	struct ReplayerData* data = (struct ReplayerData*)user_data;
-	ASAP_Delete(data->song);
-	data->song = NULL;
-	return 0;
+    struct ReplayerData* data = (struct ReplayerData*)user_data;
+    ASAP_Delete(data->song);
+    data->song = NULL;
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // TODO: These checks needs to be made much better (sanity check some more sizes in the pattern etc)
 
-enum HippoProbeResult asap_probe_can_play(const uint8_t* data, uint32_t data_size, const char* filename, uint64_t total_size) {
+enum HippoProbeResult asap_probe_can_play(const uint8_t* data, uint32_t data_size, const char* filename,
+                                          uint64_t total_size) {
     ASAP* song = ASAP_New();
 
-	if (ASAP_Load(song, filename, (uint8_t*)data, (int)data_size)) {
-		ASAP_Delete(song);
+    if (ASAP_Load(song, filename, (uint8_t*)data, (int)data_size)) {
+        ASAP_Delete(song);
         hp_info("Supported: %s", filename);
-	    return HippoProbeResult_Supported;
-	}
+        return HippoProbeResult_Supported;
+    }
 
-	hp_debug("Unsupported: %s", filename);
-	return HippoProbeResult_Unsupported;
+    hp_debug("Unsupported: %s", filename);
+    return HippoProbeResult_Unsupported;
 }
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-static int asap_read_data(void* user_data, void* dest, uint32_t samples_to_read) {
-	struct ReplayerData* data = (struct ReplayerData*)user_data;
+static HippoReadInfo asap_read_data(void* user_data, void* dest, uint32_t max_output_bytes,
+                                    uint32_t native_sample_rate) {
+    struct ReplayerData* data = (struct ReplayerData*)user_data;
 
-	int16_t buffer[FRAME_SIZE];
+    uint16_t samples_to_read = hippo_min(max_output_bytes / 4, FRAME_SIZE);
 
-	samples_to_read = hippo_min(samples_to_read, FRAME_SIZE);
+    int gen_samples = ASAP_Generate(data->song, (uint8_t*)dest, samples_to_read, ASAPSampleFormat_S16_L_E) / 2;
 
-	int gen_samples = ASAP_Generate(data->song, (uint8_t*)buffer, sizeof(buffer), ASAPSampleFormat_S16_L_E) / 2;
+    HippoReadInfo t = {
+        ASAP_SAMPLE_RATE,
+        gen_samples,
+        ASAPInfo_GetChannels(ASAP_GetInfo(data->song)),
+        HippoOutputType_s16
+    };
 
-	const float scale = 1.0f / 32767.0f;
-	float* output = (float*)dest;
-
-	if (ASAPInfo_GetChannels(ASAP_GetInfo(data->song)) == 2) {
-		for (int i = 0; i < gen_samples; ++i) {
-			const float v = ((float)buffer[i]) * scale;
-			*output++ = v;
-		}
-	} else {
-		for (int i = 0; i < gen_samples; ++i) {
-			const float v = ((float)buffer[i]) * scale;
-			*output++ = v;
-			*output++ = v;
-		}
-
-		gen_samples *= 2;
-	}
-
-	return gen_samples;
+    return t;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static int asap_seek(void* user_data, int ms) {
-	return 0;
+    return 0;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static int asap_metadata(const char* filename, const HippoServiceAPI* service_api) {
-	int ext_offset = 0;
-	int filename_start = 0;
-	char title_name[1024];
+    int ext_offset = 0;
+    int filename_start = 0;
+    char title_name[1024];
 
     const HippoIoAPI* io_api = HippoServiceAPI_get_io_api(service_api, HIPPO_FILE_API_VERSION);
     const HippoMetadataAPI* metadata_api = HippoServiceAPI_get_metadata_api(service_api, HIPPO_METADATA_API_VERSION);
@@ -199,32 +189,33 @@ static int asap_metadata(const char* filename, const HippoServiceAPI* service_ap
 
     const ASAPInfo* info = ASAP_GetInfo(song);
 
-	hp_info("Updating metadata for %s", filename);
+    hp_info("Updating metadata for %s", filename);
 
     HippoMetadataId index = HippoMetadata_create_url(metadata_api, filename);
 
     const char* title = ASAPInfo_GetTitle(info);
-	const char* extension = find_extension(filename, &ext_offset);
+    const char* extension = find_extension(filename, &ext_offset);
 
-	if (title == NULL || title[0] == 0) {
-		const char* filename_only = find_filename_start(filename, &filename_start);
-		strcpy(title_name, filename_only);
+    if (title == NULL || title[0] == 0) {
+        const char* filename_only = find_filename_start(filename, &filename_start);
+        strcpy(title_name, filename_only);
 
-		for (int i = 0; i < strlen(title_name); ++i) {
-			if (title_name[i] == '.') {
-				title_name[i] = 0;
-				break;
-			}
-		}
+        for (int i = 0; i < strlen(title_name); ++i) {
+            if (title_name[i] == '.') {
+                title_name[i] = 0;
+                break;
+            }
+        }
 
-		title = title_name;
-	}
+        title = title_name;
+    }
 
-	if (ext_offset != 0) {
-    	HippoMetadata_set_tag(metadata_api, index, HippoMetadata_SongTypeTag, ASAPInfo_GetExtDescription(extension + 1));
-	} else {
-    	HippoMetadata_set_tag(metadata_api, index, HippoMetadata_SongTypeTag, "ASAP");
-	}
+    if (ext_offset != 0) {
+        HippoMetadata_set_tag(metadata_api, index, HippoMetadata_SongTypeTag,
+                              ASAPInfo_GetExtDescription(extension + 1));
+    } else {
+        HippoMetadata_set_tag(metadata_api, index, HippoMetadata_SongTypeTag, "ASAP");
+    }
 
     HippoMetadata_set_tag(metadata_api, index, HippoMetadata_TitleTag, title);
     HippoMetadata_set_tag(metadata_api, index, HippoMetadata_ArtistTag, ASAPInfo_GetAuthor(info));
@@ -232,24 +223,24 @@ static int asap_metadata(const char* filename, const HippoServiceAPI* service_ap
     int dur = ASAPInfo_GetDuration(info, 0);
 
     if (dur != -1) {
-		HippoMetadata_set_tag_f64(metadata_api, index, HippoMetadata_LengthTag, dur / 1000.f);
-	} else {
-		HippoMetadata_set_tag_f64(metadata_api, index, HippoMetadata_LengthTag, 0.0f);
-	}
+        HippoMetadata_set_tag_f64(metadata_api, index, HippoMetadata_LengthTag, dur / 1000.f);
+    } else {
+        HippoMetadata_set_tag_f64(metadata_api, index, HippoMetadata_LengthTag, 0.0f);
+    }
 
-	int songs = ASAPInfo_GetSongs(info);
+    int songs = ASAPInfo_GetSongs(info);
 
     if (songs > 1) {
         for (int i = 0; i < songs; ++i) {
-        	float duration = ASAPInfo_GetDuration(info, i) / 1000.0f;
+            float duration = ASAPInfo_GetDuration(info, i) / 1000.0f;
             char subsong_name[1024] = {0};
-			sprintf(subsong_name, "%s (%d/%d)", title, i + 1, songs);
+            sprintf(subsong_name, "%s (%d/%d)", title, i + 1, songs);
 
-			if (duration > 0.0f) {
-            	HippoMetadata_add_subsong(metadata_api, index, i, subsong_name, duration);
-			} else {
-            	HippoMetadata_add_subsong(metadata_api, index, i, subsong_name, 0.0f);
-			}
+            if (duration > 0.0f) {
+                HippoMetadata_add_subsong(metadata_api, index, i, subsong_name, duration);
+            } else {
+                HippoMetadata_add_subsong(metadata_api, index, i, subsong_name, 0.0f);
+            }
         }
     }
 
@@ -269,34 +260,33 @@ static void asap_event(void* user_data, const unsigned char* data, int len) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static void asap_set_log(struct HippoLogAPI* log) {
-	g_hp_log = log;
+    g_hp_log = log;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static HippoPlaybackPlugin g_asap_plugin = {
-	HIPPO_PLAYBACK_PLUGIN_API_VERSION,
-	"asap",
-	"0.0.1",
-	"ASAP " ASAPInfo_VERSION,
-	asap_probe_can_play,
-	asap_supported_extensions,
-	asap_create,
-	asap_destroy,
-	asap_event,
-	asap_open,
-	asap_close,
-	asap_read_data,
-	asap_seek,
-	asap_metadata,
+    HIPPO_PLAYBACK_PLUGIN_API_VERSION,
+    "asap",
+    "0.0.1",
+    "ASAP " ASAPInfo_VERSION,
+    asap_probe_can_play,
+    asap_supported_extensions,
+    asap_create,
+    asap_destroy,
+    asap_event,
+    asap_open,
+    asap_close,
+    asap_read_data,
+    asap_seek,
+    asap_metadata,
     asap_set_log,
-	NULL,
-	NULL,
+    NULL,
+    NULL,
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 extern HIPPO_EXPORT HippoPlaybackPlugin* hippo_playback_plugin() {
-	return &g_asap_plugin;
+    return &g_asap_plugin;
 }
-
