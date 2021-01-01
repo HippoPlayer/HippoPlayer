a7a0f05999c7be36f62dc2b67ff703f632381ce5
Daniel Collin <daniel@collin.com>
2021-01-01 13:50:02 +0100

    cleanup

--- src/hippo_player/playback_plugin_panel.cpp
+++ src/hippo_player/playback_plugin_panel.cpp
@@ -120,29 +120,59 @@ void PlaybackPluginPanel::change_plugin(QTreeWidgetItem* curr, QTreeWidgetItem* 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void PlaybackPluginPanel::build_ui(const HSSetting* setting, int count) {
+    // first calculate the longest text so we can align the ui
+
+    const char* long_name = nullptr;
+    size_t max_len = 0;
+
+    for (int i = 0; i < count; ++i) {
+        HSBase* base = (HSBase*)setting;
+        size_t len = strlen(base->name);
+
+        if (max_len < len) {
+            long_name = base->name;
+            max_len = len;
+        }
+    }
+
+    QLabel dummy;
+
+    // From QLabel we figure out how much distance we need
+
+    int pixel_width = dummy.fontMetrics().boundingRect(QString::fromUtf8(long_name)).width();
+
+    //printf("pixel width %d long name %s\n", pixel_width, long_name);
+    pixel_width = 300;
+
     for (int i = 0; i < count; ++i) {
         HSBase* base = (HSBase*)setting;
 
-        QHBoxLayout* layout = new QHBoxLayout;
         QLabel* label_text = new QLabel(QString::fromUtf8(base->name));
 
-        layout->addWidget(label_text);
+        QGridLayout* layout = new QGridLayout;
+        layout->addWidget(label_text, i, 0);
+        //layout->setRowStretch(1, 2000);
+        //layout->setRowStretch(2, 2000);
+        layout->setColumnMinimumWidth(0, pixel_width);
+        layout->setColumnStretch(1, 0);
+
+        QString tool_tip = QString::fromUtf8(base->desc);
 
         switch (base->widget_type) {
             case HS_FLOAT_TYPE: {
                 QDoubleSpinBox* spin_box = new QDoubleSpinBox;
-
+                spin_box->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
                 spin_box->setValue(setting->float_value.start_value);
 
                 if (setting->float_value.start_range != setting->float_value.end_range) {
                     spin_box->setRange(setting->float_value.start_range, setting->float_value.end_range);
                 }
 
-                spin_box->setToolTip(QString::fromUtf8(base->desc));
+                spin_box->setToolTip(tool_tip);
                 spin_box->setSingleStep(0.1);
                 spin_box->setStepType(QAbstractSpinBox::AdaptiveDecimalStepType);
 
-                layout->addWidget(spin_box);
+                layout->addWidget(spin_box, i, 1);
 
                 break;
             }
@@ -150,53 +180,75 @@ void PlaybackPluginPanel::build_ui(const HSSetting* setting, int count) {
             case HS_INTEGER_TYPE: {
                 QSpinBox* spin_box = new QSpinBox;
 
-                spin_box->setValue(setting->int_value.start_value);
+
+                spin_box->setToolTip(tool_tip);
+                spin_box->setSingleStep(1);
+                spin_box->setStepType(QAbstractSpinBox::AdaptiveDecimalStepType);
+                layout->addWidget(spin_box, i, 1);
 
                 if (setting->int_value.start_range != setting->int_value.end_range) {
                     spin_box->setRange(setting->int_value.start_range, setting->int_value.end_range);
+                    spin_box->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+
+                    layout->setColumnStretch(1, 0);
+
+                    // if we have a range add a slider also
+
+                    QSlider* slider = new QSlider(Qt::Horizontal);
+                    slider->setToolTip(tool_tip);
+                    slider->setMinimum(setting->int_value.start_range);
+                    slider->setMaximum(setting->int_value.end_range);
+                    layout->addWidget(slider, i, 2);
+                } else {
+                    spin_box->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
                 }
 
-                spin_box->setToolTip(QString::fromUtf8(base->desc));
-                spin_box->setSingleStep(1);
-                spin_box->setStepType(QAbstractSpinBox::AdaptiveDecimalStepType);
+                spin_box->setValue(setting->int_value.start_value);
+                printf("start value %d\n", setting->int_value.start_value);
 
-                layout->addWidget(spin_box);
+                //QObject::connect(ui->lightOff_R_LE, &QLineEdit::textChanged, [this](const QString& val)->void{ui->lightOff_R_S->setValue(val.toInt());});
+	            //QQbject::connect(ui->lightOff_R_S, &QSlider::valueChanged,[this](const int& val)->void{ui->lightOff_R_LE->setText(locale().toString(val));});
+
+
                 break;
             }
 
             case HS_INTEGER_RANGE_TYPE: {
                 QComboBox* combo_box = new QComboBox;
+                combo_box->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
                 const HSIntegerFixedRange* range = &setting->int_fixed_value;
 
                 for (int i = 0; i < range->values_count; ++i) {
                     combo_box->addItem(QString::fromUtf8(range->values[i].name), QVariant(range->values[i].value));
                 }
 
-                combo_box->setToolTip(QString::fromUtf8(base->desc));
-                layout->addWidget(combo_box);
+                combo_box->setToolTip(tool_tip);
+                layout->addWidget(combo_box, i, 1);
                 break;
             }
 
             case HS_STRING_RANGE_TYPE: {
                 QComboBox* combo_box = new QComboBox;
                 const HSStringFixedRange* range = &setting->string_fixed_value;
+                combo_box->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
 
                 for (int i = 0; i < range->values_count; ++i) {
                     combo_box->addItem(QString::fromUtf8(range->values[i].name),
                                        QVariant(QString::fromUtf8(range->values[i].value)));
                 }
 
-                combo_box->setToolTip(QString::fromUtf8(base->desc));
-                layout->addWidget(combo_box);
+                combo_box->setToolTip(tool_tip);
+                layout->addWidget(combo_box, i, 1);
                 break;
             }
 
             case HS_BOOL_TYPE: {
                 QCheckBox* check_box = new QCheckBox;
                 const HSBool* b_value = &setting->bool_value;
+                check_box->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
                 check_box->setCheckState(b_value->value ? Qt::Checked : Qt::Unchecked);
-                check_box->setToolTip(QString::fromUtf8(base->desc));
-                layout->addWidget(check_box);
+                check_box->setToolTip(tool_tip);
+                layout->addWidget(check_box, i, 1);
                 break;
             }
 
